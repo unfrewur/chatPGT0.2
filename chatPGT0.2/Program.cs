01dcd43a5c2dd93c88ddfb1d958769b644f4d3ec
User <User@DESKTOP-2KSDJ29>
2023-09-23 14:08:39 +0300

    Add project files.

---
+++ chatPGT0.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chatPGT0._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
